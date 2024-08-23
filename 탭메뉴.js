83afdd6368566b25c8a0fb1499f3e4e9b2e77e61
JEONG-JINA <119642667+JEONG-JINA@users.noreply.github.com>
2024-08-23 09:28:23 +0900

    탭메뉴 추가

--- 탭메뉴.js
+++ 탭메뉴.js
@@ -1,3 +1,33 @@
+tabList.forEach((list, listInx) => {
+    list.addEventListener('click', (e) => {
+        const tabClass = e.target.getAttribute('class');  // dfssfsdf
+
+        document.querySelector('.tabList li.on').classList.remove('on');
+        e.target.classList.add('on');
+
+        tabConts.forEach((cont, contInx), () => {
+            if (cont.classList.contains(tabClass)) {
+                cont.style.display = 'block';
+            } else {
+                cont.style.display = 'none';
+            }
+        });
+    });
+});
+
+
+tabList.forEach((list, listInx) => {
+    list.addEventListener('click', (e) => {
+        document.querySelector('.tabList li.on').classList.remove('on');
+        e.target.classList.add('on');
+
+        tabConts.forEach((cont, contInx) => {
+            cont.classList.toggle('on', contInx === listInx);
+        });
+    });
+});
+
+
 tabBtn.forEach(function(item, index) {
     item.addEventListener('click', function() {
         tabBtn.forEach(function(tabItem, tabIndex) {
